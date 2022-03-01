d5fea02bcaed84fa9f59c81c828fff608e885427
bioss7 <fireleny@gmail.com>
2022-03-01 20:01:47 +0700

    L_11_scope_chains

---
+++ L_11_scope_chains/ex_01.js
@@ -0,0 +1,22 @@
+// Область видимости переменной scope
+// Цепочки областей видимости scope chain
+// Глобальные и локальные переменные
+
+// Глобальная 
+let i = 5; // можно убрать let
+// Локальная 
+let func = function() {
+    let i = 10;
+    console.log("func:", i); // 10
+    let innerFunc = function(){ // первая область вызова цепочки
+        let i = 15;
+        console.log("innerFunc:", i);
+    };
+    innerFunc();
+    console.log("func:", i);
+};
+
+func();
+
+// Только функции создают локальные переменные function scope
+// Объявление переменной, hosting
